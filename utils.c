c95698d7dff490fb40d8563417d2c993082be74a
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2023-09-20 21:02:08 +0100

    fix

---
+++ utils.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+int _erratoi(char *s)
+{
+	int i = 0;
+	unsigned long int result = 0;
+
+	for (i = 0;  s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+			if (result > INT_MAX)
+				return (-1);
+		}
+		else
+			return (-1);
+	}
+	return (result);
+}
