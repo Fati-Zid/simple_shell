5e253797c5dca0f275e8a4f5ab4316f1b8b32914
souad <arziki.souad@gmail.com>
2023-09-20 18:30:47 -0700

    function xitfn

--- builtin.c
+++ builtin.c
@@ -7,34 +7,20 @@
  */
 int exitfn(context_t *ctx)
 {
-	(void)ctx;
+	int exitcode;
+
 	if (ctx->cmd->argv[1])
 	{
-		exitcheck = _erratoi(ctx->cmd->argv[1]);
-		if (exitcheck == -1)
+		exitcode = _erratoi(ctx->cmd->argv[1]);
+		if (exitcode == -1)
 		{
 			_putserror(ctx, "Illegal number: ");
-			_putsln(ctx->cmd->argv[1]);
+			_eputs(ctx->cmd->argv[1]);
+			_eputs("\n");
+			ctx->status = 2;
 			return (1);
 		}
-		ctx->status = exitcheck;
+		ctx->status = exitcode;
 	}
 	return (-3);
-}
-
-/**
- * envfn - Prints the environment.
- * @ctx: The shell context.
- * Return: Always 0.
- */
-int envfn(context_t *ctx)
-{
-	list_t *h = ctx->env;
-
-	while (h != NULL)
-	{
-		_putsln(h->data ? h->data : "(nil)");
-		h = h->next;
-	}
-	return (0);
 }
