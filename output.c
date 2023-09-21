39f25b66d30272c163ea36be683cf1ac6c1afe7c
souad <arziki.souad@gmail.com>
2023-09-20 17:39:03 -0700

    warning spaces

--- output.c
+++ output.c
@@ -11,15 +11,15 @@ void _puts(const char *str)
 
 void _putsln(const char *str)
 {
-       	_puts(str);
+	_puts(str);
 	_puts("\n");
 }
 
 void _putserror(context_t *ctx, const char *error)
 {
-    _puts(ctx->pname);
+	_puts(ctx->pname);
 	_puts(": 1: ");
-    _puts(ctx->cmd->name);
-    _puts(": ");
-    _puts(error);
+	_puts(ctx->cmd->name);
+	_puts(": ");
+	_puts(error);
 }
